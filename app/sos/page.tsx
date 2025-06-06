b00aafaa493edb3dbdb6067077dbaa7b850ee73f
AbhishekKumar2002 <lit2021049@iiitl.ac.in>
2025-06-06 23:31:59 +0530

    SOS

---
+++ app/sos/page.tsx
@@ -0,0 +1,12 @@
+import SosButton from '@/components/SosButton'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mt-20'>
+        <SosButton />
+    </div>
+  )
+}
+
+export default page
