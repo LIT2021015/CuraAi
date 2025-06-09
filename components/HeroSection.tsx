7d1d0777280957a4bece79a4440247e31db0c421
LIT2021015 <lit2021015@iiitl.ac.in>
2025-06-09 08:49:26 +0530

    build 10

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,12 +4,12 @@ const HeroSection = () => {
   return (
     <div className="mt-32 flex flex-col items-center justify-center ml-5 text-[#333333] dark:text-[#E6E6E6]">
       <h1 className="text-4xl font-bold">
-        SmartDiagnose - an all-in-one medical solution
+        SmartDiagnose - a Unified Health-Tech Platform
       </h1>
       <br />
       <h1 className="text-xl text-center max-w-4xl">
-        <b>SmartDiagnose</b> brings 7 disease detections like Covid, Brain
-        Tumor, Breast Cancer, Alzheimer, Diabetes, Pneumonia, and Heart Disease
+        <b>SmartDiagnose</b> brings 6 disease detections like Covid, Brain
+        Tumor, Breast Cancer, Diabetes, Pneumonia, and Heart Disease
         under one platform.
       </h1>
     </div>
